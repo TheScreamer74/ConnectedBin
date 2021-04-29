4ab4bef22fd75c2bb6ee20bc573ce4bc29d1fe3e
Thomas MLD <thuyghues@myluckyday.fr>
2021-04-29 15:56:38 +0200

    Initializing project

---
+++ app/src/main/java/com/example/connectbin/presentation/ui/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.connectbin.presentation.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import com.example.connectbin.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //val binding: MainActivityBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+    /*
+        val navController = (supportFragmentManager.findFragmentById(R.id.main_fragment_container_view) as NavHostFragment).navController
+        binding.mainBottomNavigationView.setupWithNavController(navController)*/
+
+    }
+}
