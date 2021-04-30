087509922ae8ff7a63912728666d8d731ea630ef
TheScreamer74 <blakilleur@gmail.com>
2021-04-30 15:05:15 +0200

    finished project set up + starting bottom nav view

--- app/src/main/java/com/example/connectbin/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/connectbin/presentation/ui/MainActivity.kt
@@ -5,12 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import com.example.connectbin.R
+import com.example.connectbin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //val binding: MainActivityBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
     /*
         val navController = (supportFragmentManager.findFragmentById(R.id.main_fragment_container_view) as NavHostFragment).navController
